b057b6ca6662cfbd974b7e4549d3c4af61eefd91
Aman Srivastava <haptik-74@Haptik-74s-MacBook-Air.local>
2019-05-31 18:08:41 +0530

    added-more-utils-function-in-embedding

--- models/utils.py
+++ models/utils.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import collections
 
 _Embeddings = collections.namedtuple('Embedding', ['name', 'dimensions', 'corpus_size', 'vocabulary_size',
-                                                   'download_url', 'format', 'architecture', 'trained_data'
+                                                   'download_url', 'format', 'architecture', 'trained_data',
                                                    ])
 
 
@@ -12,6 +12,9 @@ class Embeddings(object):
     _lookup_corpus_size = {m.name: m.corpus_size for m in _members}
     _lookup_vocab_size = {m.name: m.vocabulary_size for m in _members}
     _lookup_dimensions = {m.name: m.dimensions for m in _members}
+    _lookup_format = {m.name: m.format for m in _members}
+    _lookup_architecture = {m.name: m.architecture for m in _members}
+    _lookup_trained_data = {m.name: m.trained_data for m in _members}
     _lookup_download_url = {m.name: m.download_url for m in _members}
 
     @classmethod
@@ -29,6 +32,18 @@ class Embeddings(object):
     @classmethod
     def get_dimensions(cls, name):
         return cls._lookup_dimensions[name]
+
+    @classmethod
+    def get_format(cls, name):
+        return cls._lookup_format[name]
+
+    @classmethod
+    def get_architecture(cls, name):
+        return cls._lookup_architecture[name]
+
+    @classmethod
+    def get_trained_data(cls, name):
+        return cls._lookup_trained_data[name]
 
     @classmethod
     def get_all(cls):
