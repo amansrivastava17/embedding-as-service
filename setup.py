cb132ed42bf2dd007d4ac37c61db9264a8d03dfa
amansrivastava17 <amans.rlx@gmail.com>
2019-08-30 17:56:13 +0530

    updated readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="embedding_as_service",
-    version="0.4.0",
+    version="0.5.0",
     author="Aman Srivastava",
     author_email="amans.rlx@gmail.com",
     description="embedding-as-service: one-stop solution to encode sentence to vectors using various embedding methods",
