42bc9549750d1a5852da24177950d1d77dd59f22
DhavalTaunk08 <taunkdhaval08@gmail.com>
2019-09-07 23:11:20 +0530

    add-keras-feedforward-layer

---
+++ embedding_as_service/custom_layer/keras_custom_layer.py
@@ -0,0 +1,24 @@
+from keras import backend as K
+from keras.layers import Layer
+
+
+class DenseLayer(Layer):
+    def __init__(self, input_shape=64, activation='relu', output_dim=64, **kwargs):
+        self.input_shape = input_shape
+        self.activation = activation
+        self.output_dim = output_dim
+        super(DenseLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        self.dense = Layer.Dense(units=self.output_dim,
+                                 activation=self.activation,
+                                 trainable=True)
+
+        super(DenseLayer, self).build(input_shape)
+
+    def call(self, x):
+        return K.dot(x, self.dense)
+
+    def compute_output_shape(self):
+        return self.output_dim
