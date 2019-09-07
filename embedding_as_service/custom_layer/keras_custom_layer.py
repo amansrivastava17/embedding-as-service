82cd2fe9643d489f510eca6810f9f761824149c9
DhavalTaunk08 <taunkdhaval08@gmail.com>
2019-09-08 00:00:02 +0530

    add-parameters-customization

--- embedding_as_service/custom_layer/keras_custom_layer.py
+++ embedding_as_service/custom_layer/keras_custom_layer.py
@@ -3,20 +3,22 @@ from keras.layers import Layer
 
 
 class DenseLayer(Layer):
-    def __init__(self, **kwargs):
-
+    def __init__(self, input_shape, output_dim, activation, **kwargs):
+        self.input_shape = input_shape,
+        self.output_dim = output_dim,
+        self.activation = activation
         super(DenseLayer, self).__init__(**kwargs)
 
-    def build(self, input_shape, output_dim, activation):
+    def build(self):
 
-        self.dense = Layer.Dense(units=output_dim,
-                                 activation=activation,
+        self.dense = Layer.Dense(units=self.output_dim,
+                                 activation=self.activation,
                                  trainable=True)
 
-        super(DenseLayer, self).build(input_shape, output_dim, activation)
+        super(DenseLayer, self).build()
 
     def call(self, x):
         return K.dot(x, self.kernel)
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
+        return (self.input_shape[0], self.output_dim)
